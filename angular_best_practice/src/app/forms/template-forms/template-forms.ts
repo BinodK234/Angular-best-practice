6481204fc8d9f834e3d1e9bba7078f2a87fe618d
unknown <sahubinodkumar03@gmail.com>
2026-06-07 16:26:16 +0530

    Practiced the Template driven form completely.

--- angular_best_practice/src/app/forms/template-forms/template-forms.ts
+++ angular_best_practice/src/app/forms/template-forms/template-forms.ts
@@ -1,11 +1,19 @@
+import { JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-template-forms',
-  imports: [],
+  imports: [FormsModule, JsonPipe],
   templateUrl: './template-forms.html',
   styleUrl: './template-forms.scss',
 })
 export class TemplateForms {
+  email: string = '';
+  password: string = ''
 
+
+onSubmit(form: NgForm){
+  console.log(form.value);
+}
 }
